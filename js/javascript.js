ef3fe655c2eeb9de7bd9780c695cb8265a29048d
sks4world2 <58714508+sks4world2@users.noreply.github.com>
2020-07-13 00:32:30 -0400

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,27 @@
+
+
+function openTab(tabName) {
+	var i, x; 
+
+	x = document.getElementsByClassName("containerTab");
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	document.getElementById(tabName).style.display = "block";
+	
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+	acc[i].addEventListener("click", function() {
+		this.classList.toggle("active");
+		var panel = this.nextElementSibling;
+		if (panel.style.display === "block") {
+			panel.style.display = "none"; 
+		} else {
+			panel.style.display = "block";
+		}
+	});
+}
